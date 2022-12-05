175dbbaf2c5c2048adba79c13e87c480116ee150
irem <iremaydinn.7@gmail.com>
2022-12-05 17:19:06 +0300

    ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //to give starting force
+    public Vector2 startForce;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
