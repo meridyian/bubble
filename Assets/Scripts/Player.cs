a5455a63e99e3fdc4a0c139dca97b6d6e4051a66
irem <iremaydinn.7@gmail.com>
2022-12-05 17:08:52 +0300

    get input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 4f;
+    public Rigidbody2D rb;
+    private float movement = 0f;
     void Update()
     {
+        // (-1,1)
+        
+        movement  = Input.GetAxisRaw("Horizontal") * speed ;
+    }
+
+    void FixedUpdate()
+    {
+        //fixedupdate physics calculated
+        //current+movement
+        rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+        
+        
         
     }
 }
