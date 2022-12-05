002fb8248d74bc8549966b86c5d0740200617f4d
irem <iremaydinn.7@gmail.com>
2022-12-05 17:59:27 +0300

    chain script

--- Assets/Scripts/Chain.cs
+++ Assets/Scripts/Chain.cs
@@ -7,6 +7,7 @@ public class Chain : MonoBehaviour
     //to keep it same even if levels change
     public static bool isFired ;
     public Transform player;
+    public float speed = 2f;
     void Start()
     {
         isFired = false;
@@ -15,14 +16,19 @@ public class Chain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown(("Fire1")))
+        {
+            isFired = true;
+        }
         if (isFired)
         {
-            
+            transform.localScale = transform.localScale + Vector3.up * Time.deltaTime * speed;
         }
         else
         {
             //since pivots are aligned 
             transform.position = player.position;
+            transform.localScale = new Vector3(1f, 0f, 1f);
         }
     }
 }
