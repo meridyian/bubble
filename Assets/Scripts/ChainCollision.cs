9f9991621d7bddf4b11c0af3cdddaf3484aacdb0
irem <iremaydinn.7@gmail.com>
2022-12-06 09:14:35 +0300

    split

--- Assets/Scripts/ChainCollision.cs
+++ Assets/Scripts/ChainCollision.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ChainCollision : MonoBehaviour
 {
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D col)
     {
         //we have 1 player, 
         Chain.isFired = false;
+
+        if (col.tag == "Ball")
+        {
+            
+        }
     }
 }
